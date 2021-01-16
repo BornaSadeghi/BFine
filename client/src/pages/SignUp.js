b3cabf4ac3f4c6f890442d72034e695dc70cb46c
eric-prog <eric.sprog@gmail.com>
2021-01-16 10:58:49 -0500

    frontend structure and skeleton code

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/SignUp.css';
+
+function SignUp() {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+        </div>
+    )
+}
+
+export default SignUp;
