ef396e0a7365f2be856192dac765aa3b48882a53
BornaSadeghi <sadeghiborna@gmail.com>
2021-01-16 16:17:21 -0500

    Update all endpoints

--- app.js
+++ app.js
@@ -22,34 +22,63 @@ app.get('/', (req, res) => {
     res.send('base url');
 })
 
-// Authentication endpoint
+// ENDPOINTS
 
 /**
- * Compare login credentials to database credentials, and if they match, allow access
+ * Takes the mobile number and user type (donor/bloodbank) and responds with a verification code, sent
+ * to the user through SMS.
  */
-app.post('/auth', bodyParser.json(), (req, res) => {
+app.post('/auth/init', bodyParser.json(), (req, res) => {
     console.log(req.body);
-    res.send('/auth is hit');
-})
-
-// User creation endpoints
-
-/**
- * Create a new donor user and add it to the database.
- */
-app.post('/newuser-donor', bodyParser.json(), (req, res) => {
-    const user = req.body;
-    console.log(user);
-    res.send('/newuser-donor is hit');
+    res.send('/auth/init is hit');
 })
 
 /**
- * Create a new receiver user and add it to the database.
+ * Takes verification code and phone number, responds with session id and if user is already registered
+ * (yes/no)
  */
-app.post('/newuser-receiver', bodyParser.json(), (req, res) => {
-    const user = req.body;
-    console.log(user);
-    res.send('/newuser-reciever is hit');
+app.post('/auth/establish', bodyParser.json(), (req, res) => {
+    console.log(req.body);
+    res.send('/auth/establish is hit');
+})
+
+/**
+ * Takes an object with donor fields, updates donor in database, responds true or false
+ */
+app.post('/updateDonorProfile', bodyParser.json(), (req, res) => {
+    console.log(req.body);
+    res.send('/updateDonorProfile is hit');
+})
+
+/**
+ * Takes bank stock object, updates bank in database, responds true or false
+ */
+app.post('/updateBankStock', bodyParser.json(), (req, res) => {
+    console.log(req.body);
+    res.send('/updateBankStock is hit')
+})
+
+/**
+ * Takes lat and long of donor, responds with best locations by priority (distance and stock)
+ */
+app.post('/search', bodyParser.json(), (req, res) => {
+    console.log(req.body);
+    res.send('/search is hit')
+})
+
+/**
+ * Takes UrgentRequest object, returns true or false
+ */
+app.post('/urgent/make', bodyParser.json(), (req, res) => {
+    console.log(req.body);
+    res.send('/updateBankStock is hit')
+})
+
+// GET
+
+app.get('/urgent/details/:id', bodyParser.json(), (req, res) => {
+    console.log(`body: ${req.body}, params: ${req.params}`);
+    res.send('/urgent/details/<id> is hit')
 })
 
 app.listen(PORT, () => {
