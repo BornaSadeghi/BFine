e256de9076c75265dc54098ad2c6e946fd508cf9
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2021-01-17 18:17:47 +0530

    services - reqs

--- services/requests.js
+++ services/requests.js
@@ -5,7 +5,7 @@ const bloodBankModel = require('../models/bloodBank');
 const requestModel = require('../models/urgentRequest');
 const reqs = {};
 
-reqs.make = (body) => {
+reqs.make = async(body) => {
     return new Promise(async(resolve) => {
         try {
             await requestModel.create(body);
@@ -16,8 +16,29 @@ reqs.make = (body) => {
     })
 }
 
-reqs.close = (id, number) => {
-    requestModel.find({_id:id}, async(err, docs) => {
-        
-    }); 
+reqs.close = async(id, number) => {
+    return new Promise(async(resolve) => {
+        requestModel.find({_id:id}, async(err, docs) => {
+            if (!err){
+                let doc = docs[0];
+                doc.status = 'closed';
+    
+                bloodBankModel.find({_id: doc.bloodBankId}, async(err, banks) => { 
+                    if (!err) {
+                        await message.sms(`Donor found for urgent request, please contact at ${number}`, banks[0].phoneNumber);
+                        try {
+                            await requestModel.updateOne({ _id: doc._id }, doc);
+                            resolve({res: true});
+                        } catch (error) {
+                            resolve({res: false, reason: e});
+                        }
+                    } else {
+                        resolve({res: false, reason: 'err'});
+                    }
+                });
+            }
+        }); 
+    })
 }
+
+module.exports = reqs;
